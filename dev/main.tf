bb236d6cee64be050da8ce4d6a27a1bd8ffad47b
Andrew Barger <asbarg01@gmail.com>
2024-04-07 18:38:38 -0400

    Debug (pit)

--- dev/main.tf
+++ dev/main.tf
@@ -12,7 +12,6 @@ terraform {
         container_name       = "tfstate-container"
         key                  = "dev.terraform.tfstate"
         use_oidc = true
-        #use_azuread_auth     = true
         subscription_id      = "8b28b648-3c7c-4959-8daa-b9fad32b7b67"
         tenant_id            = "7f0754a3-7e98-4f2a-8435-fd93b7fdd02a"
     }
